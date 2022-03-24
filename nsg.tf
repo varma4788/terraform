2aec11009d31d60b4e3deb9c8c257e43d93e1b34
varma4788 <vijayraju.indukuri93@gmail.com>
2022-03-24 11:09:54 +0530

    NSG

---
+++ nsg.tf
@@ -0,0 +1,19 @@
+resource "azurerm_network_security_group" "mytfnsg" {
+  name                = "var.nsgname"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
