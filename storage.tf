93a99cb5c2ccdacb029ff539d152ad0ced712803
varma4788 <vijayraju.indukuri93@gmail.com>
2022-03-24 17:12:01 +0530

    storage

---
+++ storage.tf
@@ -0,0 +1,16 @@
+resource "random_id" "randomID" {
+    keepers = {
+      "resource_group" = "azurerm_resource_group.myTerraformGroup.name"
+    }
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "tfstorage" {
+  name                     = "diag${random_id.randomId.hex}"
+  resource_group_name      = azurerm_resource_group.myTerraformGroup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = var.tags
+}
